e50e8180339295d7e8a3dcbdfa4a6ba35bf128fc
diegokauer <diego.kauer@ug.uchile.cl>
2025-05-09 09:29:01 -0400

    init commit

--- setup.py
+++ setup.py
@@ -4,20 +4,17 @@ setup(
     name="phylo_gnn",
     version="0.1.0",
     packages=find_packages(),
-    # install_requires=[
-    #     # Add your dependencies here, e.g.,
-    #     # 'torch>=1.11.0',
-    #     'numpy>=1.24.4',
-    #     'setuptools>=73.0.1',
-    #     'pandas>=2.0.3',
-    #     'pillow>=10.3.0',
-    #     'torch>=2.4.1',
-    #     'torchvision>=0.19.1',
-    #     'tqdm>=4.62.3',
-    #     'matplotlib>=3.7.5',
-    #     'scikit-learn>=1.3.2',
-    #     'pycox>=0.3.0'
-    # ],
+    install_requires=[
+        # Add your dependencies here, e.g.,
+        'torch',
+        'torch_geometric',
+        'dotenv',
+        'pandas',
+        'ete3',
+        'networkx',
+        'numpy',
+        'composition_stats'
+    ],
     # entry_points={
     #     'console_scripts': [
     #         'mirai-chile-predict = mirai_chile.script:main',
